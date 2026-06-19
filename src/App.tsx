5775e4a760403bedca0f7a712dbeaf65fd67c231
JACOB STANLEY <jaynomjacobstanley@gmail.com>
2026-06-19 10:05:38 +0100

    https://github.com/CredenceOrg/Credence-Frontend.git

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,37 @@
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import ToastProvider from './components/ToastProvider'
 import Layout from './components/Layout'
-import { Suspense, lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import RouteLoader from './components/RouteLoader';
+import RouteLoader from './components/RouteLoader'
 
-const Home = lazy(() => import('./pages/Home'));
-const Bond = lazy(() => import('./pages/Bond'));
-const TrustScore = lazy(() => import('./pages/TrustScore'));
-const AmountInputTestPage = lazy(() => import('./pages/AmountInputTestPage'));
-const FocusTrapTestPage = lazy(() => import('./pages/FocusTrapTestPage'));
-const NotFound = lazy(() => import('./pages/NotFound'));
+const Home = lazy(() => import('./pages/Home'))
+const Bond = lazy(() => import('./pages/Bond'))
+const TrustScore = lazy(() => import('./pages/TrustScore'))
+const AmountInputTestPage = lazy(() => import('./pages/AmountInputTestPage'))
+const FocusTrapTestPage = lazy(() => import('./pages/FocusTrapTestPage'))
+const NotFound = lazy(() => import('./pages/NotFound'))
+
+function RouteChangeManager() {
+  const location = useLocation()
+
+  React.useEffect(() => {
+    // Scroll to top on route change
+    window.scrollTo(0, 0)
+
+    // Focus on the main heading (h1) for accessibility
+    const mainHeading = document.querySelector('h1')
+    if (mainHeading) {
+      mainHeading.setAttribute('tabindex', '-1')
+      mainHeading.focus()
+    }
+  }, [location.pathname])
+
+  return null
+}
 
 function AppRoutes() {
   return (
     <Suspense fallback={<RouteLoader />}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bond" element={<Bond />} />
-        <Route path="/trust-score" element={<TrustScore />} />
-        <Route path="/test/amount-input" element={<AmountInputTestPage />} />
-        <Route path="/test/focus-trap" element={<FocusTrapTestPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Suspense>
-  );
-}
-
-export default AppRoutes;
-function App() {
-  return (
-    <BrowserRouter>
-      <ToastProvider>
-import { Suspense, lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-
-const Home = lazy(() => import('./pages/Home'));
-const Bond = lazy(() => import('./pages/Bond'));
-const TrustScore = lazy(() => import('./pages/TrustScore'));
-const AmountInputTestPage = lazy(() => import('./pages/AmountInputTestPage'));
-const FocusTrapTestPage = lazy(() => import('./pages/FocusTrapTestPage'));
-const NotFound = lazy(() => import('./pages/NotFound'));
-
-function AppRoutes() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -58,10 +43,15 @@ function AppRoutes() {
         </Route>
       </Routes>
     </Suspense>
-  );
+  )
 }
 
-export default AppRoutes;
+function App() {
+  return (
+    <BrowserRouter>
+      <ToastProvider>
+        <RouteChangeManager />
+        <AppRoutes />
       </ToastProvider>
     </BrowserRouter>
   )
